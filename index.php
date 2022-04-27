84b67154b80f5d6b9af5fd4d4ea69da593e6e8ab
halyna20 <halynka2000@gmail.com>
2022-04-27 17:39:24 +0300

    Add template

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 include "class/Student.php";
 include "class/Aspirant.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-$aspirant = new Aspirant('Ivan', 'Veresen', '123', 5, 'Scientific work');
-$student = new Student('Oleg', 'Hrinko', '13', 4);
-$students = [$student, $aspirant];
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-foreach ($students as $item) {
-    $item->getScholarship();
-    echo '<br/>';
-}
+<body>
+    <?php
+    $aspirant = new Aspirant('Ivan', 'Veresen', '123', 5, 'Scientific work');
+    $student = new Student('Oleg', 'Hrinko', '13', 4);
+    $students = [$student, $aspirant];
+
+    foreach ($students as $item) {
+        $item->getScholarship();
+        echo '<br />';
+    }
+    ?>
+</body>
+
+</html>
